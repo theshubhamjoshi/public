0bb15e9e595b3dab255b53b3c7938a33b2a80f55
smartron1 <38962193+smartron1@users.noreply.github.com>
2020-04-11 22:20:24 +0530

    Delete firebase.js

--- firebase.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "AIzaSyCf3p9eOXIsRkgxtfj-pYvPZaTpvuEfd80",
-    authDomain: "amritha-bb49d.firebaseapp.com",
-    databaseURL: "https://amritha-bb49d.firebaseio.com",
-    projectId: "amritha-bb49d",
-    storageBucket: "amritha-bb49d.appspot.com",
-    messagingSenderId: "468918862141"
-  };
-  firebase.initializeApp(config);
